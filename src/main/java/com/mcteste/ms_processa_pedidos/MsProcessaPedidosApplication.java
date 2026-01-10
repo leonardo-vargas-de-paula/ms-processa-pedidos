386688597ee996aefd9c3858fd26ff75092c5de0
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-10 12:40:42 -0400

    first commit

---
+++ src/main/java/com/mcteste/ms_processa_pedidos/MsProcessaPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.mcteste.ms_processa_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProcessaPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProcessaPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcteste/ms_processa_pedidos/MsProcessaPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcteste.ms_processa_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProcessaPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
