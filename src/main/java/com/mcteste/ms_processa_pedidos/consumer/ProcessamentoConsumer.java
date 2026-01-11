4daf8d3bb1e7861f0cf1d1b2229176c786279a1e
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-10 20:10:27 -0400

    feat: adiciona pedidodto no listener

--- src/main/java/com/mcteste/ms_processa_pedidos/consumer/ProcessamentoConsumer.java
+++ src/main/java/com/mcteste/ms_processa_pedidos/consumer/ProcessamentoConsumer.java
@@ -1,5 +1,6 @@
 package com.mcteste.ms_processa_pedidos.consumer;
 
+import com.mcteste.ms_processa_pedidos.dto.PedidoDTO;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class ProcessamentoConsumer {
 
     @RabbitListener(queues = "${broker.queue.processamento.name}")
-    public void listernerProcesamentoQueue(@Payload String descricao){
-        System.out.println("Teste descrição = "+descricao);
+    public void listernerProcesamentoQueue(PedidoDTO pedidoDTO){
+        System.out.println("Teste descrição = "+pedidoDTO.getDescricao());
     }
 }
